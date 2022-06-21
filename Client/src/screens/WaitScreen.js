d2aa88d304998af480d23e2668ce3dec7aea571e
AllenAlexander98 <73653888+AllenAlexander98@users.noreply.github.com>
2022-06-22 04:29:39 +0700

    better

--- Client/src/screens/WaitScreen.js
+++ Client/src/screens/WaitScreen.js
@@ -2,21 +2,23 @@ import React from "react";
 
 function WaitScreen() {
   return (
-    <div id="wait-game" className="container vh-100">
-      <div>
-        <main className="row mt-5">
-          <div>
-            <h1 className="text-center">You're in!</h1>
-            <h5 className="text-center">See your nickname on screen?</h5>
-          </div>
-        </main>
-        <footer className="row mt-5">
-          <div className="d-flex justify-content-between">
-            <h1>Name</h1>
-            <h1>0</h1>
-          </div>
-        </footer>
-      </div>
+    <div
+      id="wait-game"
+      className="container vh-100 d-flex flex-column justify-content-between"
+    >
+      <div></div>
+      <main className="row">
+        <div>
+          <h1 className="text-center">You're in!</h1>
+          <h5 className="text-center">See your nickname on screen?</h5>
+        </div>
+      </main>
+      <footer className="row">
+        <div className="d-flex justify-content-between">
+          <h1>Name</h1>
+          <h1>0</h1>
+        </div>
+      </footer>
     </div>
   );
 }
